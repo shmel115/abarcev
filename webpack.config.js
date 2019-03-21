dbc0263d361856804547cf03dfcc941229cc66e3
abarcev <a.barczev@yandex.ru>
2019-03-21 16:51:36 +0300

    module reviews

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
   const buildDir = "./docs";
 
+
   const pcss = {
     test: /\.(p|post|)css$/,
     use: [
